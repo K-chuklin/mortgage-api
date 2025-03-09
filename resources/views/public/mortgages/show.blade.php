0248b729d966eacb0d786fcf1d48544aa4e0c104
K-chuklin <inkarttatoo@gmail.com>
2025-03-10 01:19:00 +0500

    feat: Install and configure Bootstrap, update template files
    - Installed and configured Bootstrap
    - Updated `composer.json` and `composer.lock`
    - Added `package-lock.json`
    - Updated `package.json`
    - Modified `resources/js/bootstrap.js`
    - Added new SCSS files: `_variables.scss` and `app.scss`
    - Updated Blade template files: `layouts/app.blade.php` and `public/mortgages/show.blade.php`
    - Modified `vite.config.js`

--- resources/views/public/mortgages/show.blade.php
+++ resources/views/public/mortgages/show.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>📄 {{ $mortgage->title }}</h1>
-    <p><strong>Процентная ставка:</strong> {{ $mortgage->percent }}%</p>
-    <p><strong>Стоимость недвижимости:</strong> от {{ $mortgage->min_price }} до {{ $mortgage->max_price }}</p>
-    <p><strong>Первоначальный взнос:</strong> от {{ $mortgage->min_first_payment }}%</p>
-    <p><strong>Срок ипотеки:</strong> от {{ $mortgage->min_term }} до {{ $mortgage->max_term }} лет</p>
-    <a href="{{ route('home') }}">🔙 Назад к списку</a>
+<div class="card mt-4">
+  <div class="card-body">
+    <h5 class="card-title">📄 {{ $mortgage->title }}</h5>
+    <p class="card-text"><strong>Процентная ставка:</strong> {{ $mortgage->percent }}%</p>
+    <p class="card-text"><strong>Стоимость недвижимости:</strong> от {{ $mortgage->min_price }} до {{ $mortgage->max_price }}</p>
+    <p class="card-text"><strong>Первоначальный взнос:</strong> от {{ $mortgage->min_first_payment }}%</p>
+    <p class="card-text"><strong>Срок ипотеки:</strong> от {{ $mortgage->min_term }} до {{ $mortgage->max_term }} лет</p>
+    <a href="{{ route('home') }}" class="btn btn-outline-primary">🔙 Назад к списку</a>
+  </div>
+</div>
+
 @endsection
