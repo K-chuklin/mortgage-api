620d1af9d35e54b31ce95753492bf2b6628e94b5
K-chuklin <inkarttatoo@gmail.com>
2025-03-12 05:11:30 +0500

    Исправил edit шаблон, изменил create шаблон

--- resources/views/admin/mortgages/create.blade.php
+++ resources/views/admin/mortgages/create.blade.php
@@ -1,16 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Добавить ипотеку</h1>
-    <form action="{{ route('admin.mortgages.store') }}" method="POST">
-        @csrf
-        <input type="text" name="title" placeholder="Название" required>
-        <input type="number" name="percent" placeholder="Процент" required>
-        <input type="number" name="min_first_payment" placeholder="Мин. взнос" required>
-        <input type="number" name="max_price" placeholder="Макс. цена" required>
-        <input type="number" name="min_price" placeholder="Мин. цена" required>
-        <input type="number" name="min_term" placeholder="Мин. срок" required>
-        <input type="number" name="max_term" placeholder="Макс. срок" required>
-        <button type="submit">Создать</button>
-    </form>
+<div class="card mt-4">
+    <div class="card-body">
+        <h5 class="card-title">✏️ Добавить ипотеку</h5>
+            <form action="{{ route('admin.mortgages.store') }}" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <label class="form-label">Название</label>
+                    <input type="text" class="form-control" name="title" placeholder="Название">
+                    <div class="form-text">Полный адрес дома или квартиры</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Процентная ставка</label>
+                    <input type="number" min="1" max="99" class="form-control" name="percent" placeholder="Процент">
+                    <div class="form-text">Значение в %</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальный взнос</label>
+                    <input type="number" class="form-control" name="min_first_payment" placeholder="Мин.взнос">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Максимальная цена</label>
+                    <input type="number" class="form-control" name="max_price" placeholder="Макс. цена">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальная цена</label>
+                    <input type="number" class="form-control" name="min_price" placeholder="Мин. цена">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальный срок</label>
+                    <input type="number" min="1" class="form-control" name="min_term" placeholder="Мин. срок">
+                    <div class="form-text">Значение в годах</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Максимальный срок</label>
+                    <input type="number" max="30" class="form-control" name="max_term" placeholder="Макс. срок">
+                    <div class="form-text">Значение в годах</div>
+                </div>
+                <button type="submit" class="btn btn-outline-success">💾 Создать</button>
+            </form>
+                <a class="btn btn-outline-primary mt-2" href="{{ route('admin.mortgages.index') }}">🔙 Назад</a>
+        </div>
+    </div>
 @endsection
--- resources/views/admin/mortgages/edit.blade.php
+++ resources/views/admin/mortgages/edit.blade.php
@@ -2,91 +2,53 @@
 
 @section('content')
     <div class="card mt-4">
-    <div class="card-body">
-        <h5 class="card-title">✏️ Редактировать ипотеку: {{ $mortgage->title }}</h5>
-        <form action="{{ route('admin.mortgages.update', $mortgage->id) }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class="mb-3">
-                <label class="form-label">Название</label>
-                <input type="text" class="form-control" name="title" value="{{ $mortgage->title }}">
-                <div class="form-text">Полный адрес дома или квартиры</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Процентная ставка</label>
-                <input type="number" min="1" max="99" class="form-control" name="percent" value="{{ $mortgage->percent }}">
-                <div class="form-text">Значение в %</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Минимальный взнос</label>
-                <input type="number" class="form-control" name="min_first_payment" value="{{ $mortgage->min_first_payment }}">
-                <div class="form-text">Значение в рублях</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Максимальная цена</label>
-                <input type="number" class="form-control" name="max_price" value="{{ $mortgage->max_price }}">
-                <div class="form-text">Значение в рублях</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Минимальная цена</label>
-                <input type="number" class="form-control" name="min_price" value="{{ $mortgage->min_price }}">
-                <div class="form-text">Значение в рублях</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Минимальный срок</label>
-                <input type="number" min="1" class="form-control" name="min_term" value="{{ $mortgage->min_term }}">
-                <div class="form-text">Значение в годах</div>
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Максимальный срок</label>
-                <input type="number" max="30" class="form-control" name="max_term" value="{{ $mortgage->max_term }}">
-                <div class="form-text">Значение в годах</div>
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" name="is_active" value="1" {{ $mortgage->is_active ? 'checked' : '' }}>
-                <label class="form-check-label">Активна</label>
-            </div>
-            <button type="submit" class="btn btn-outline-success">💾 Сохранить</button>
-        </form>
-            <a class="btn btn-outline-primary mt-2" href="{{ route('admin.mortgages.index') }}">🔙 Назад</a>
+        <div class="card-body">
+            <h5 class="card-title">✏️ Редактировать ипотеку: {{ $mortgage->title }}</h5>
+            <form action="{{ route('admin.mortgages.update', $mortgage->id) }}" method="POST">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label class="form-label">Название</label>
+                    <input type="text" class="form-control" name="title" value="{{ $mortgage->title }}">
+                    <div class="form-text">Полный адрес дома или квартиры</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Процентная ставка</label>
+                    <input type="number" min="1" max="99" class="form-control" name="percent" value="{{ $mortgage->percent }}">
+                    <div class="form-text">Значение в %</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальный взнос</label>
+                    <input type="number" class="form-control" name="min_first_payment" value="{{ $mortgage->min_first_payment }}">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Максимальная цена</label>
+                    <input type="number" class="form-control" name="max_price" value="{{ $mortgage->max_price }}">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальная цена</label>
+                    <input type="number" class="form-control" name="min_price" value="{{ $mortgage->min_price }}">
+                    <div class="form-text">Значение в рублях</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Минимальный срок</label>
+                    <input type="number" min="1" class="form-control" name="min_term" value="{{ $mortgage->min_term }}">
+                    <div class="form-text">Значение в годах</div>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Максимальный срок</label>
+                    <input type="number" max="30" class="form-control" name="max_term" value="{{ $mortgage->max_term }}">
+                    <div class="form-text">Значение в годах</div>
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" name="is_active" value="1" {{ $mortgage->is_active ? 'checked' : '' }}>
+                    <label class="form-check-label">Активна</label>
+                </div>
+                <button type="submit" class="btn btn-outline-success">💾 Сохранить</button>
+            </form>
+                <a class="btn btn-outline-primary mt-2" href="{{ route('admin.mortgages.index') }}">🔙 Назад</a>
+        </div>
     </div>
-    </div>
-
-
-    <h1>✏️ Редактировать ипотеку</h1>
-
-    <form action="{{ route('admin.mortgages.update', $mortgage->id) }}" method="POST">
-        @csrf
-        @method('PUT')
-
-        <label>Название</label>
-        <input type="text" name="title" value="{{ $mortgage->title }}" required>
-
-        <label>Процентная ставка</label>
-        <input type="number" name="percent" value="{{ $mortgage->percent }}" required>
-
-        <label>Мин. взнос</label>
-        <input type="number" name="min_first_payment" value="{{ $mortgage->min_first_payment }}" required>
-
-        <label>Макс. цена</label>
-        <input type="number" name="max_price" value="{{ $mortgage->max_price }}" required>
-
-        <label>Мин. цена</label>
-        <input type="number" name="min_price" value="{{ $mortgage->min_price }}" required>
-
-        <label>Мин. срок</label>
-        <input type="number" name="min_term" value="{{ $mortgage->min_term }}" required>
-
-        <label>Макс. срок</label>
-        <input type="number" name="max_term" value="{{ $mortgage->max_term }}" required>
-
-        <label>
-            <input type="checkbox" name="is_active" value="1" {{ $mortgage->is_active ? 'checked' : '' }}>
-            Активна
-        </label>
-
-        <button type="submit">💾 Сохранить</button>
-    </form>
-
-    <a href="{{ route('admin.mortgages.index') }}">🔙 Назад</a>
 @endsection
